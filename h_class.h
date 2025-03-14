d5b5b99eebed3164f6c8d56141d6f51d87c906cd
YoMan <42252155+YoMan12@users.noreply.github.com>
2025-03-14 21:44:49 +0100

    Add files via upload

--- h_class.h
+++ h_class.h
@@ -9,55 +9,6 @@ const char *show(bool isShown) {
   return isShown ? "block\">" : "none\">";
 }
 
-// //---------------------------------------------------------------------------//
-// //                                                                   buttonPara                                                           
-// class buttonPara : public Supla::HtmlElement { 
-//  public:
-//   buttonPara()
-//     : HtmlElement(HTML_SECTION_FORM) {}
-
-//   void send(Supla::WebSender* sender) {
-//     auto cfg = Supla::Storage::ConfigInstance();
-    
-//     if (cfg) {
-
-//       // - ▼ - ▼ - ▼ - ▼ - ▼ - ▼ - ▼ - ▼ - ▼ - ▼ - ▼ - ▼ - ▼ - ▼ - ▼ - ▼ - ▼
-//       sender->send("<div class=\"form-field\">");
-//       sender->sendLabelFor(FILTER_KEY, "Filter");
-//       sender->send(
-//               "<input type=\"number\" min=\"0\" max=\"");
-//       sender->send(10000);
-//       sender->send("\" step=\"1\" ");
-//       sender->sendNameAndId(FILTER_KEY);
-//       sender->send(" value=\"");
-//       inCfgValue = 800;
-//       cfg->getUInt32(FILTER_KEY, &inCfgValue);
-//       sender->send(inCfgValue);
-//       sender->send("\">");
-//       sender->send("</div>");
-//       // - ▲ - ▲ - ▲ - ▲ - ▲ - ▲ - ▲ - ▲ - ▲ - ▲ - ▲ - ▲ - ▲ - ▲ - ▲ - ▲ - ▲
-      
-//     }  // if cfg
-//   }    // sender
-
-//   bool handleResponse(const char* key, const char* value) {
-//     auto cfg = Supla::Storage::ConfigInstance();
-    
-//     if (cfg && strcmp(key, FILTER_KEY) == 0) {
-//       uint32_t inFormValue = stringToUInt(value);
-
-//       cfg->setUInt32(FILTER_KEY, inFormValue);
-//       filter = inFormValue;
-//       return true;
-//     }        
-
-//     return false;
-//   }
-
-//  protected:
-//   uint32_t inCfgValue = 0;
-// };  // class buttonPara
-
 //---------------------------------------------------------------------------//
 //                                                                   myCheckBox                                                          
 class myCheckBox : public Supla::HtmlElement { 
